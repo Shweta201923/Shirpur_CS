5a72a5990afa69c7ebec76bf79fee211b7f9b896
Sandeep M Waghmare <113245211+Shweta201923@users.noreply.github.com>
2022-09-16 20:36:14 +0530

    Basic

---
+++ Day1.py
@@ -0,0 +1,66 @@
+'''
+5 days
+Python Functions:-
+Python OOPs:-
+
+DSA :- Linear
+Test1
+DSA :- Non Linear
+Test2
+Sorting
+DBMS
+Test3
+
+'''
+
+'''
+Q1:-
+Python devloper
+
+Other prog vs python?
+C,C++,Java vs Python
+1)print("Hello")
+
+1)Hardware 1lakhs loc i3 5th gen vs i7 10th gen
+
+4.32 secs :- complier
+3.98/4 sec :-  python
+
+Q2:- Data types  
+list = [],
+set = {},
+tuple = (),
+dict = {}
+'''
+# list = "Sandeep"
+# print(list)
+#Q3 :-  set vs dict
+list1 = []
+set1 = {}
+tuple1 = ()
+dict1 = {}
+
+# print(type(list1))
+# print(type(set1))
+# print(type(tuple1))
+# print(type(dict1))
+
+
+# List tuple dict set
+
+# Q4:- in python everything is based on classes true or false?Justify
+
+# name = "Harry"
+# print(type(name))
+
+# Tuple only one element type type of that particular elemnt
+# new_tuple = 1
+new_tuple = (1,)
+
+new = tuple(new_tuple)
+print(type(new))
+print(new)
+print(new_tuple)
+# result = tuple(iterable,var_iter)
+
+
